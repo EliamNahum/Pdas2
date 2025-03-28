0b07c5794d9eafe967354650732374c342a7e973
Eliam Nahum <braca.eliam@gmail.com>
2025-03-27 19:05:35 -0700

    Canbio en profileCard con algunas propiedades

--- src/ProfileCard.jsx
+++ src/ProfileCard.jsx
@@ -1,10 +1,26 @@
-function ProfileCard(props) {
-    console.log(props)
+function ProfileCard({title, handle, image}) {
   return (
-    <div>ProfileCard</div>
+  <div className="card">
+      <div className="card-image">
+        <figure className="image-is-lbyl">
+          <img src={image} alt="" width={200} height={200}/>
+        </figure>
+      </div>
+
+      <div className="card-content">
+        <div className="media-content">
+          <p className="titleis-4">Title is{title}</p>
+          <p className="subtitle is-6">Handle is{handle}</p>
+        </div>
+      </div>
+  </div>
   )
 }
 
 export default ProfileCard
+
+
+
+
 
 
